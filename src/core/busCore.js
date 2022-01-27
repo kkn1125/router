497110e2bc0ffb207b1cd143e92593f8ed5b566e
kkn1125 <xpfjrnseks1@naver.com>
2022-01-27 23:55:48 +0900

    version downgrade

---
+++ src/core/busCore.js
@@ -0,0 +1,3 @@
+import {Router, Route, App, Layout} from 'https://cdn.jsdelivr.net/gh/kkn1125/router@854420c/src/core/core.js'
+
+export {Router, Route, App, Layout}
--- src/core/core.js
+++ src/core/core.js
@@ -1,7 +1,7 @@
 'use strict';
 const App = {
     brand: 'Router',
-    version: '0.1.1'
+    version: '0.1.0'
 }
 
 const base404 = {
