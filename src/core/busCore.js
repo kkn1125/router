be5e8926872bc9fdb3601c21daf482dc527e9830
kkn1125 <xpfjrnseks1@naver.com>
2022-01-28 00:42:12 +0900

    busCore로 버그수정

---
+++ src/core/busCore.js
@@ -0,0 +1,9 @@
+import {router, route, app, layout} from 'https://cdn.jsdelivr.net/gh/kkn1125/router@7c2441b/src/core/core.js'
+// import {router, route, app, layout} from './core.js'
+
+const Router = router;
+const Route = route;
+const App = app;
+const Layout = layout;
+
+export { Router, Route, App, Layout }
